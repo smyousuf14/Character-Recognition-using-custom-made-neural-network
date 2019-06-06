317042f67678dc1db05a4b1c2859d3fe64c4a9a6
smyousuf14 <44485548+smyousuf14@users.noreply.github.com>
2019-06-06 16:55:41 -0400

    Add files via upload

--- characterRecognition.py
+++ characterRecognition.py
@@ -10,11 +10,11 @@ class Network:
         self.num_layers = len(sizes)
         self.sizes = sizes
         self.biases = [np.random.randn(y,1) for y in sizes[1:]]
-        self.wieghts = [np.random.randn(y,x) for x,y in zip(sizes[:-1], sizes[1:])]
+        self.weights = [np.random.randn(y,x) for x,y in zip(sizes[:-1], sizes[1:])]
 
     def feedforward(self, a):
         #Return the output of the network if "a" is input.
-        for b,w in zip(self.biases,  self.wieghts):
+        for b,w in zip(self.biases,  self.weights):
             a = sigmoid(np.dot(w,a) + b)
         return a
 
@@ -23,18 +23,18 @@ class Network:
         if test_data:
             n_test = len(test_data)
         n = len(training_data)
-        for j in range(epochs):
+        for j in xrange(epochs):
             random.shuffle(training_data)
             mini_batches = [
                 training_data[k:k+mini_batch_size]
-                for k in range(0, n, mini_batch_size)]
+                for k in xrange(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
             if test_data:
-                print("Epoch [0]: [1] / [2]".format(
+                print("Epoch {0}: {1} / {2}".format(
                     j, self.evaluate(test_data), n_test))
             else:
-                print("Epoch [0] complete")
+                print(("Epoch {0} complete").format(j))
 
     def update_mini_batch(self, mini_batch, eta):
         #update the networks weights and biases by applying gradient descent using backpropogation
@@ -45,8 +45,8 @@ class Network:
             delta_nabla_b, delta_nabla_w = self.backprop(x,y)
             nabla_b = [nb + dnb for nb,dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw + dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-        self.wieghts = [w-(eta/len(mini_batch)) * nw
-                        for w,nw in zip(self.wieghts, nabla_w)]
+        self.weights = [w-(eta/len(mini_batch)) * nw
+                        for w,nw in zip(self.weights, nabla_w)]
         self.biases = [b - (eta/len(mini_batch)) * nb
                        for b,nb in zip(self.biases, nabla_b)]
     def backprop(self,x,y):
@@ -68,7 +68,7 @@ class Network:
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
 
-        for l in range(2, self.num_layers):
+        for l in xrange(2, self.num_layers):
             z = zs[-l]
             sp = sigmoid_prime(z)
             delta = np.dot(self.weights[-l + 1].transpose(), delta) * sp
@@ -93,3 +93,7 @@ def sigmoid(z):
 def sigmoid_prime(z):
     #Derivative of the sigmoid function
     return sigmoid(z) * (1-sigmoid(z))
+
+# -------------------------------------------------------------------------------------------------
+# Now create a program that can recognize handwritten digits
+
